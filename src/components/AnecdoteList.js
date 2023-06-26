26e4683d33eb62db0386f58e5a5b14ed9acb6db7
Kristian Hannula <kristian.hannula@proton.me>
2023-06-26 17:36:05 +0300

    Task 6.9 done, added filtering

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -2,20 +2,29 @@ import { useSelector } from 'react-redux'
 import Anecdote from './Anecdote'
 
 const Anecdotes = () => {
-  const anecdotes = useSelector(state => [...state].sort((a, b) => {
-    return b.votes - a.votes
-  }))
+  const filter = useSelector(state => state.filter)
 
-return (
-  <div>
-    {anecdotes.map(anecdote =>
-      <Anecdote
-        key={anecdote.id}
-        anecdote={anecdote}
-      />
-    )}
-  </div>
-)
+  const anecdotes = useSelector(state =>
+    state.anecdotes
+      .sort((a, b) => b.votes - a.votes)
+      .filter(anecdote => {
+        if (filter === 'ALL') {
+          return true
+        }
+        return anecdote.content.includes(filter)
+      })
+  )
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+        />
+      )}
+    </div>
+  )
 }
 
 export default Anecdotes
